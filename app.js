cd20a8908615e5fd7f8dfddcf99f6c760b35dce0
Ellexandrei Esponilla <xandrei37@gmail.com>
2023-03-05 19:28:49 +0800

    addded scripts

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
-let choice = ["rock", "paper", "scissors"];
+let choice = ["rock", "paper", "scissor"];
 
 function getComputerChoice() {
-  return choice[Math.floor(Math.random() * 3) + 1];
+  return choice[Math.floor(Math.random() * 3)];
 }
 
-function Game(playerChoice, computerChoice) {
-  let player = playerChoice.toLowerCase();
-  let computer = computerChoice.toLowerCase();
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "tie";
+    //
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return "computer wins";
+    } else {
+      return "human wins";
+    }
+    //
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissor") {
+      return "computer wins";
+    } else {
+      return "player wins";
+    }
+    //
+  } else if (playerSelection === "scissor") {
+    if (computerSelection === "rock") {
+      return "computer wins";
+    } else {
+      return "player wins";
+    }
+  }
+}
+
+function Game() {
+  const playerSelection = prompt("Choose what hand you'll pick");
+  const computerSelection = getComputerChoice();
+
+  console.log(playRound(playerSelection, computerSelection));
+}
+
+for (let x = 0; x < 5; x++) {
+  Game();
 }
